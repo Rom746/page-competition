964cb3f9ace5e610865fed2d7d631423f892ca75
Roman Kulikov <kulikovroman746@gmail.com>
2022-07-25 23:02:03 +0300

    add

---
+++ src/components/CheckBox/CheckBox.tsx
@@ -0,0 +1,40 @@
+import AccentLink from 'components/AccentLink';
+import React, { FC } from 'react';
+import { IChildClass } from 'utils/constants/types';
+import join from 'utils/joinClasses';
+import './CheckBox.scss';
+
+interface CheckBoxProps extends IChildClass {
+  type: 'primary' | 'secondary';
+}
+
+const CheckBox: FC<CheckBoxProps> = ({ children, type, className}) => {
+  const index = 'checkbox' + Math.random();
+  const joinType: any = 'additional-' + type;
+  return (
+    <div className={join('checkbox', className)}>
+      <input type="checkbox"
+        name={index}
+        className='checkbox__input'
+      />
+      <label htmlFor={index} className='checkbox__label'>
+        {
+          children ? children : (
+            <p>
+              {'Отправляя данные я соглашаюсь с '}
+              <AccentLink src='/' type={joinType}>
+                Условиями конкурса
+              </AccentLink>
+              {' и '}
+              <AccentLink src='/' type={joinType}>
+                Политикой обработки данных
+              </AccentLink>
+            </p>
+          )
+        }
+      </label>
+    </div>
+  );
+};
+
+export default CheckBox;
--- src/container/Registration/FormRegistration.tsx
+++ src/container/Registration/FormRegistration.tsx
@@ -3,22 +3,27 @@ import Button from 'components/Button';
 import images from 'utils/constants/images';
 import React, { FC } from 'react';
 import Input from 'components/Input';
+import CheckBox from 'components/CheckBox';
 
 const FormRegistration: FC = () => {
-   return (
-       <form className='registration__form'>
-        <h2 className='registration__title'>Зарегистрируйтесь, чтобы участвовать</h2>
-        <BlockFlex className='registration__inputs'>
-          <Input placeholder='ФИО ребёнка'/>
-        </BlockFlex>
+  return (
+    <form className='registration__form'>
+      <h2 className='registration__title'>Зарегистрируйтесь, чтобы участвовать</h2>
+      <BlockFlex className='registration__inputs'>
+        <Input placeholder='ФИО ребёнка' />
+        <Input placeholder='Дата рождения' />
+        <Input placeholder='Город' />
+        <Input placeholder='Email' type='email' />
+        <Input placeholder='Пароль' type='password' />
+      </BlockFlex>
+      <CheckBox type='primary' className='registration__check'/>
 
-
-        <BlockFlex className='registration__buttons'>
-          <Button className='registration__button' type='main-secondary'>Учавствовать</Button>
-          <img src={images.planes} alt="Planes" />
-        </BlockFlex>
-       </form>
-   );
+      <BlockFlex className='registration__buttons'>
+        <Button className='registration__button' type='main-secondary'>Учавствовать</Button>
+        <img src={images.planes} alt="Planes" />
+      </BlockFlex>
+    </form>
+  );
 };
 
 export default FormRegistration;
